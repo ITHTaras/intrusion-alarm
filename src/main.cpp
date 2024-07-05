5790fba877b14e984c608b854b695b3ec186e8e7
Taras Hornik <tarasgorniko@gmail.com>
2024-07-05 21:42:47 +0200

    IR Control Demo

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,30 @@
 #include <Arduino.h>
+#include <IRremote.h>
 
 unsigned long currentMillis;
 unsigned long previousMillis = 0;
 
-short int doorPin = 2;
+int RECV_PIN = 11;
+int doorPin = 2;
 bool reedState;
+bool IrEnabled = false;
 
 int resetPin = 3;
 bool resetState;
 bool opened = false;
 
 // notes in the melody:
-short int melody[]{530, 2000};
-short int currentFrequency = 0;
+int alarmPin = 4;
+int melody[]{530, 2000};
+int currentFrequency = 0;
 size_t melodySize = sizeof(melody) / sizeof(melody[0]);
-unsigned short int toneDuration = 500;
+unsigned int toneDuration = 500;
 
 void setup()
 {
   pinMode(doorPin, INPUT);
   pinMode(resetPin, INPUT);
-  // Serial.begin(9600);
+  Serial.begin(9600);
 }
 
 void loop()
@@ -34,12 +38,88 @@ void loop()
   if (opened && resetState)
   {
     opened = false;
+
+    IrReceiver.begin(RECV_PIN, DISABLE_LED_FEEDBACK);
+    IrEnabled = true;
+  }
+  if (IrEnabled && IrReceiver.decode())
+  {
+    // Print Code in HEX
+    switch (IrReceiver.decodedIRData.decodedRawData)
+    {
+    case 0xBA45FF00:
+      Serial.println("POWER");
+      break;
+    case 0xB847FF00:
+      Serial.println("FUNC/STOP");
+      break;
+    case 0xB946FF00:
+      Serial.println("VOL+");
+      break;
+    case 0xBB44FF00:
+      Serial.println("FAST BACK");
+      break;
+    case 0xBF40FF00:
+      Serial.println("PAUSE");
+      break;
+    case 0xBC43FF00:
+      Serial.println("FAST FORWARD");
+      break;
+    case 0xF807FF00:
+      Serial.println("DOWN");
+      break;
+    case 0xEA15FF00:
+      Serial.println("VOL-");
+      break;
+    case 0xF609FF00:
+      Serial.println("UP");
+      break;
+    case 0xE619FF00:
+      Serial.println("EQ");
+      break;
+    case 0xF20DFF00:
+      Serial.println("ST/REPT");
+      break;
+    case 0xE916FF00:
+      Serial.println("0");
+      break;
+    case 0xF30CFF00:
+      Serial.println("1");
+      break;
+    case 0xE718FF00:
+      Serial.println("2");
+      break;
+    case 0xA15EFF00:
+      Serial.println("3");
+      break;
+    case 0xF708FF00:
+      Serial.println("4");
+      break;
+    case 0xE31CFF00:
+      Serial.println("5");
+      break;
+    case 0xA55AFF00:
+      Serial.println("6");
+      break;
+    case 0xBD42FF00:
+      Serial.println("7");
+      break;
+    case 0xAD52FF00:
+      Serial.println("8");
+      break;
+    case 0xB54AFF00:
+      Serial.println("9");
+      break;
+    default:
+      Serial.println("error");
+    }
+    IrReceiver.resume();
   }
   // N.C. Reed Switch
   if (opened && currentMillis - previousMillis >= toneDuration)
   {
     previousMillis = currentMillis;
-    tone(4, melody[currentFrequency], toneDuration);
+    tone(alarmPin, melody[currentFrequency], toneDuration);
     currentFrequency = currentFrequency == 0 ? 1 : 0;
   }
   else if (!reedState)
