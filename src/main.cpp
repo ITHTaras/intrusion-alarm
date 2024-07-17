4c8123353acdd6a5dd0fc8830d0acc2331d3bcbd
Taras Hornik <tarasgorniko@gmail.com>
2024-07-17 12:28:12 +0200

    displaying code

--- src/main.cpp
+++ src/main.cpp
@@ -4,32 +4,55 @@
 #include <Arduino.h>
 #include <IRremote.h>
 #include <vector>
+#include <LiquidCrystal.h>
 
 unsigned long currentMillis;
 unsigned long previousMillis = 0;
+unsigned long previousMillisLCD = 0;
 
-const int RECV_PIN = 11;
+const int RECV_PIN = 9;
 std::vector<int> code;
 const int combination = 2749;
 
-int doorPin = 2;
+int doorPin = 8;
 bool reedState;
 
 bool opened = false;
 
 // notes in the melody:
-int alarmPin = 4;
+int alarmPin = 10;
 int melody[]{530, 2000};
 int currentFrequency = 0;
 size_t melodySize = sizeof(melody) / sizeof(melody[0]);
 unsigned int toneDuration = 500;
 
+LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
+int refreshSpan = 100;
+
+void printCode(int currentMillis)
+{
+  if (currentMillis - previousMillisLCD >= refreshSpan)
+  {
+    lcd.setCursor(0, 0);
+    previousMillisLCD = currentMillis;
+    if (code.size() == 1)
+      lcd.print(code[0]);
+    else if (code.size() == 2)
+      lcd.print(code[0] * 10 + code[1]);
+    else if (code.size() == 3)
+      lcd.print(code[0] * 100 + code[1] * 10 + code[2]);
+    else if (code.size() == 4)
+      lcd.print(code[0] * 1000 + code[1] * 100 + code[2] * 10 + code[3]);
+  }
+}
+
 void setup()
 {
   pinMode(doorPin, INPUT);
 
-  // Serial.begin(9600);
   IrReceiver.begin(RECV_PIN);
+
+  lcd.begin(0, 0);
 }
 
 void loop()
@@ -38,81 +61,96 @@ void loop()
 
   reedState = digitalRead(doorPin);
 
-  // if (code.size() == 1)
-  //   Serial.println(code[0]);
-  // else if (code.size() == 2)
-  //   Serial.println(code[0] * 10 + code[1]);
-  // else if (code.size() == 3)
-  //   Serial.println(code[0] * 100 + code[1] * 10 + code[2]);
-  // else if (code.size() == 4)
-  //   Serial.println(code[0] * 1000 + code[1] * 100 + code[2] * 10 + code[3]);
+  printCode(currentMillis);
 
   // N.C. Reed Switch
-  if (opened && currentMillis - previousMillis >= toneDuration)
+  if (opened)
   {
-    previousMillis = currentMillis;
-    tone(alarmPin, melody[currentFrequency], toneDuration);
-    currentFrequency = currentFrequency == 0 ? 1 : 0;
+    // Ton abspielen
+    if (currentMillis - previousMillis >= toneDuration)
+    {
+      previousMillis = currentMillis;
+      tone(alarmPin, melody[currentFrequency], toneDuration);
+      currentFrequency = currentFrequency == 0 ? 1 : 0;
+    }
 
-    if (reedState  && IrReceiver.decode())
-  {
-    // Print Code in HEX
-    switch (IrReceiver.decodedIRData.decodedRawData)
+    // IR-Signal empfangen
+    if (reedState && IrReceiver.decode())
     {
-    case 0xBA45FF00:
-    {
-      int codeNumber = code.size() == 4 ? code[0] * 1000 + code[1] * 100 + code[2] * 10 + code[3] : 0;
-      if (code.size() == 4 && codeNumber == combination && reedState)
-        opened = false;
-      break;
+      // Print Code in HEX
+      switch (IrReceiver.decodedIRData.decodedRawData)
+      {
+      case 0xBA45FF00:
+      {
+        int codeNumber = code.size() == 4 ? code[0] * 1000 + code[1] * 100 + code[2] * 10 + code[3] : 0;
+        if (code.size() == 4 && codeNumber == combination && reedState)
+        {
+          opened = false;
+          if (toneDuration != 500)
+          {
+            toneDuration = 500;
+            melody[0] -= 300;
+            melody[1] -= 500;
+          }
+        }
+
+        else if (code.size() == 4 && codeNumber != combination)
+        {
+          toneDuration = 250;
+          melody[0] += 300;
+          melody[1] += 500;
+          code.clear();
+        }
+        break;
+      }
+      case 0xBB44FF00:
+        if (code.size() != 0)
+          code.clear();
+        lcd.clear();
+        break;
+      case 0xE916FF00:
+        if (code.size() != 4)
+          code.push_back(0);
+        break;
+      case 0xF30CFF00:
+        if (code.size() != 4)
+          code.push_back(1);
+        break;
+      case 0xE718FF00:
+        if (code.size() != 4)
+          code.push_back(2);
+        break;
+      case 0xA15EFF00:
+        if (code.size() != 4)
+          code.push_back(3);
+        break;
+      case 0xF708FF00:
+        if (code.size() != 4)
+          code.push_back(4);
+        break;
+      case 0xE31CFF00:
+        if (code.size() != 4)
+          code.push_back(5);
+        break;
+      case 0xA55AFF00:
+        if (code.size() != 4)
+          code.push_back(6);
+        break;
+      case 0xBD42FF00:
+        if (code.size() != 4)
+          code.push_back(7);
+        break;
+      case 0xAD52FF00:
+        if (code.size() != 4)
+          code.push_back(8);
+        break;
+      case 0xB54AFF00:
+        if (code.size() != 4)
+          code.push_back(9);
+        break;
+      }
+      IrReceiver.resume();
     }
-    case 0xBB44FF00:
-      if (code.size() != 0)
-        code.clear();
-      break;
-    case 0xE916FF00:
-      if (code.size() != 4)
-        code.push_back(0);
-      break;
-    case 0xF30CFF00:
-      if (code.size() != 4)
-        code.push_back(1);
-      break;
-    case 0xE718FF00:
-      if (code.size() != 4)
-        code.push_back(2);
-      break;
-    case 0xA15EFF00:
-      if (code.size() != 4)
-        code.push_back(3);
-      break;
-    case 0xF708FF00:
-      if (code.size() != 4)
-        code.push_back(4);
-      break;
-    case 0xE31CFF00:
-      if (code.size() != 4)
-        code.push_back(5);
-      break;
-    case 0xA55AFF00:
-      if (code.size() != 4)
-        code.push_back(6);
-      break;
-    case 0xBD42FF00:
-      if (code.size() != 4)
-        code.push_back(7);
-      break;
-    case 0xAD52FF00:
-      if (code.size() != 4)
-        code.push_back(8);
-      break;
-    case 0xB54AFF00:
-      if (code.size() != 4)
-        code.push_back(9);
-      break;
-    }
-    IrReceiver.resume();
-  }
   }
   else if (!reedState)
   {
